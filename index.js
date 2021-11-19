d110dc9ce1e9ec7286243194e810e4f69879ef9f
Esther Kang <eskang87@gmail.com>
2021-11-19 13:39:35 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "<p>Esther is the champion </p>";
